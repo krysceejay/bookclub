3b17d6ce860aa5bf0a0f652cfa89452df4ab4f1a
Chris <chrischimachijioke@gmail.com>
2019-08-15 20:35:58 +0100

    user_controller change

--- lib/bookclub_web/controllers/user_controller.ex
+++ lib/bookclub_web/controllers/user_controller.ex
@@ -1,7 +1,7 @@
 defmodule BookclubWeb.UserController do
   use BookclubWeb, :controller
 
-    plug BookclubWeb.Plugs.RequireAuth
+  plug BookclubWeb.Plugs.RequireAuth
 
   def index(conn, _params) do
     render(conn, "dashboard.html")
