231e5a0426d73e310718b8adfdf8324246226353
krysceejay <chrischimachijioke@gmail.com>
2019-11-05 16:25:28 +0100

    still on responsive chat room

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,48 +1,53 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  const chatdivh = $("#chat-body-id").prop("scrollHeight");
   chatDivScroll();
   chatDivChange();
 
   // $("#chatareaform").submit(function(){
   //   chatDivScrollT();
   // });
+  // const hasFocus = $("#chat-area-input").is(":focus");
+  // if (hasFocus) {
+  //   chatDivScroll();
+  // }
 
-  function chatDivScroll(){
-      const chatdivh = $("#chat-body-id").prop('scrollHeight');
-      $("#chat-body-id").scrollTop(chatdivh);
+  function chatDivScroll() {
+    $("#chat-body-id").scrollTop(chatdivh);
   }
 
-  function chatDivScrollT(){
-      const chatdivh = $("#chat-body-id").prop('scrollHeight');
-      //const chath = $("#chat-body-id").height();
-      setTimeout(function(){
-         $("#chat-body-id").scrollTop(chatdivh);
-       }, 100);
-     }
+  function chatDivScrollT() {
+    setTimeout(function() {
+      $("#chat-body-id").scrollTop(chatdivh);
+    }, 100);
+  }
 
-     function chatDivChange(){
-         $("#chat-body-id").bind("DOMSubtreeModified", function(){
-           chatDivScrollT();
-         });
-     }
+  function chatDivChange() {
+    $("#chat-body-id").bind("DOMSubtreeModified", function() {
+      chatDivScrollT();
+    });
+  }
 
-     $("#users-close-btn").click(function () {
-       $(".chat-container-online-users").hide();
-     });
+  $("#users-close-btn").click(function() {
+    $(".chat-container-online-users").hide();
+  });
 
-     $("#online-users-show").click(function () {
-       $(".chat-container-online-users").show();
-     });
+  $("#online-users-show").click(function() {
+    $(".chat-container-online-users").show();
+  });
 
-     $("#detail-close-btn").click(function () {
+  $("#detail-close-btn").click(function() {
+    $(".chat-container-bookdetails").hide();
+  });
 
-       $(".chat-container-bookdetails").hide();
-     });
+  $("#book-details-show").click(function() {
+    $(".chat-container-bookdetails").show();
+  });
 
-     $("#book-details-show").click(function () {
+  // $("#chat-area-input").focus(function() {
 
-       $(".chat-container-bookdetails").show();
-     });
-
-
-
+  // });
+  $(window).resize(function() {
+    chatDivScroll();
+    chatDivChange();
+  });
 });
