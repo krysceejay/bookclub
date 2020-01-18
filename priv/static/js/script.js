8ced103f1f8f8c2ef93998d9b62ed44d876e9a81
krysceejay <chrischimachijioke@gmail.com>
2020-01-18 18:49:23 +0100

    user dashboard design

--- priv/static/js/script.js
+++ priv/static/js/script.js
@@ -33,4 +33,130 @@ $(document).ready(function() {
   //   //alert(strid);
   // });
 
+  const options1 = {
+         series: [{
+         name: 'Books',
+         data: [2.3, 3.1, 4.0, 10.1]
+       }],
+         chart: {
+         height: 350,
+         type: 'bar',
+       },
+       plotOptions: {
+         bar: {
+           dataLabels: {
+             position: 'top', // top, center, bottom
+           },
+         }
+       },
+       dataLabels: {
+         enabled: true,
+         formatter: function (val) {
+           return val + "%";
+         },
+         offsetY: -20,
+         style: {
+           fontSize: '15px',
+           colors: ["#304758"]
+         }
+       },
+
+       xaxis: {
+         categories: ["Public", "Private", "Completed", "Still Reading"],
+         position: 'top',
+         labels: {
+           offsetY: -18,
+
+         },
+         axisBorder: {
+           show: false
+         },
+         axisTicks: {
+           show: false
+         },
+         crosshairs: {
+           fill: {
+             type: 'gradient',
+             gradient: {
+               colorFrom: '#D8E3F0',
+               colorTo: '#BED1E6',
+               stops: [0, 100],
+               opacityFrom: 0.4,
+               opacityTo: 0.5,
+             }
+           }
+         },
+         tooltip: {
+           enabled: true,
+           offsetY: -35,
+
+         }
+       },
+       fill: {
+         gradient: {
+           shade: 'light',
+           type: "horizontal",
+           shadeIntensity: 0.25,
+           gradientToColors: undefined,
+           inverseColors: true,
+           opacityFrom: 1,
+           opacityTo: 1,
+           stops: [50, 0, 100, 100]
+
+         },
+         colors: ["#25A9FF"],
+       },
+       yaxis: {
+         axisBorder: {
+           show: false
+         },
+         axisTicks: {
+           show: false,
+         },
+         labels: {
+           show: true,
+           formatter: function (val) {
+             return val + "%";
+           }
+         }
+
+       },
+       title: {
+         text: 'Statistics Of Books Added',
+         floating: true,
+         offsetY: 320,
+         align: 'center',
+         style: {
+           color: '#444'
+         }
+       }
+       };
+
+       const chart1 = new ApexCharts(document.querySelector("#chart-books"), options1);
+       chart1.render();
+
+       //Pie chart
+
+        const options2 = {
+          series: [44, 55],
+          chart: {
+          type: 'donut',
+        },
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: 'bottom'
+            }
+          }
+        }],
+        colors: ["#7BE8B6", "#FF4560"],
+        };
+
+        const chart2 = new ApexCharts(document.querySelector("#chart-readers"), options2);
+        chart2.render();
+
 });
