dac0af61d0f9535d3a84ffee2dc3e6450d6174c5
krysceejay <chrischimachijioke@gmail.com>
2020-04-25 09:07:22 +0100

    finishing touches

--- priv/repo/migrations/20190708102605_create_users.exs
+++ priv/repo/migrations/20190708102605_create_users.exs
@@ -11,7 +11,7 @@ defmodule Bookclub.Repo.Migrations.CreateUsers do
       add :status, :integer, default: 0
       add :about, :text, null: true
       add :propix, :string, null: true
-      add :colour, :text, default: "#000000"
+      add :colour, :string, default: "#000000"
       add :role_id, references(:roles, on_delete: :nothing), null: false, default: 1
 
       timestamps()
