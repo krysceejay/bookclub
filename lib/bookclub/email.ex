d8b7bb65da2fde9c2240bb9e79e6fef39dd8f8c7
krysceejay <chrischimachijioke@gmail.com>
2020-04-25 16:41:04 +0100

    email template edit

--- lib/bookclub/email.ex
+++ lib/bookclub/email.ex
@@ -26,7 +26,7 @@ defmodule Bookclub.Email do
 
   defp base_email() do
     new_email()
-      |> from("mail@shelfvibe.com")
+      |> from("Shelfvibe<mail@shelfvibe.com>")
       |> put_html_layout({BookclubWeb.LayoutView, "email.html"})
   end
 
